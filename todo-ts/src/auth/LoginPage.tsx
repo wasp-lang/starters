f66a79878f84fb0287367c7e3c962b1ada9b3eae
Franjo Mindek <franjo.mindek@gmail.com>
2025-05-13 13:17:08 +0200

    Revert "readme"
    This reverts commit 86ad863670d8dc47296dfe4af00979def5778345.

---
+++ todo-ts/src/auth/LoginPage.tsx
@@ -0,0 +1,18 @@
+import { LoginForm } from "wasp/client/auth"
+// Wasp's type-safe Link component
+import { Link } from "wasp/client/router"
+
+export function LoginPage() {
+  return (
+    <main>
+      {/** Wasp has built-in auth forms & flows, which you can customize or opt-out of, if you wish :)
+       * https://wasp.sh/docs/guides/auth-ui
+       */}
+      <LoginForm />
+      <br />
+      <span>
+        I don't have an account yet (<Link to="/signup">go to signup</Link>).
+      </span>
+    </main>
+  );
+}
---
+++ todo-ts/src/auth/SignupPage.tsx
@@ -0,0 +1,18 @@
+import { SignupForm } from "wasp/client/auth"
+// Wasp's type-safe Link component
+import { Link } from "wasp/client/router"
+
+export function SignupPage() {
+  return (
+    <main>
+      {/** Wasp has built-in auth forms & flows, which you can customize or opt-out of, if you wish :)
+       * https://wasp.sh/docs/guides/auth-ui
+       */}
+      <SignupForm />
+      <br />
+      <span>
+        I already have an account (<Link to="/login">go to login</Link>).
+      </span>
+    </main>
+  );
+}
