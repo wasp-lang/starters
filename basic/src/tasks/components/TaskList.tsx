aa9fef717eda4aea7c821f93347fa492486c0b2e
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-05 17:19:37 +0200

    svg

--- basic/src/tasks/components/TaskList.tsx
+++ basic/src/tasks/components/TaskList.tsx
@@ -38,8 +38,20 @@ export function TaskList() {
           <span>{completedTasks.length} completed</span>
         </div>
         {completedTasks.length > 0 && (
-          <button className="font-medium" onClick={deleteCompletedTasks}>
-            🗑️ Clear completed
+          <button
+            className="flex items-center gap-1 font-medium"
+            onClick={deleteCompletedTasks}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              className="fill-black"
+            >
+              <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+            </svg>
+            Clear completed
           </button>
         )}
       </div>
