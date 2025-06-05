05fb72f9eed5e0dea8426dd4dbd42b6c7c8733fb
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-05 14:06:13 +0200

    fix input

--- basic/src/common/Input.tsx
+++ basic/src/common/Input.tsx
@@ -10,7 +10,7 @@ type InputProps = Omit<
 
 export function Input({ className, label, ...props }: InputProps) {
   return (
-    <div className={`flex flex-col gap-1 ${className}`}>
+    <div className="flex flex-col gap-1">
       <label htmlFor={props.id} className="text-black">
         {label}:
       </label>
