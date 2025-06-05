c0a421e70d22ce8710192273fb109fd92d281e45
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-05 17:05:27 +0200

    emoji be gone

--- basic/src/tasks/TaskDashboardPage.tsx
+++ basic/src/tasks/TaskDashboardPage.tsx
@@ -5,7 +5,7 @@ import { TaskList } from "./components/TaskList";
 export const TaskDashboardPage = ({ user }: { user: AuthUser }) => {
   return (
     <div className="flex flex-col items-center gap-12 p-12 py-24">
-      <h1 className="text-4xl font-bold">{`${user.username}'s tasks 📋`}</h1>
+      <h1 className="text-4xl font-bold">{`${user.username}'s tasks`}</h1>
       <div className="flex w-full flex-col items-center gap-8">
         <section className="card flex w-full max-w-3xl flex-col gap-6">
           <CreateTaskForm />
