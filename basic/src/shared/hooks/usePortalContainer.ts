f4b8b6cc48f553d56cab2f558c6087d29954e29f
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-05 19:43:25 +0200

    make containers have ids

--- basic/src/shared/hooks/usePortalContainer.ts
+++ basic/src/shared/hooks/usePortalContainer.ts
@@ -2,9 +2,12 @@ import React from "react";
 
 export function usePortalContainer() {
   const [container, setContainer] = React.useState<HTMLElement | null>(null);
+  const id = React.useId();
 
   React.useEffect(() => {
     const el = document.createElement("div");
+    el.id = `portal-container-${id}`;
+
     document.body.appendChild(el);
     setContainer(el);
 
